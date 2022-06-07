d7b3445b0452cda5ae94999a59c7f0b1cbe0e77e
cliyok <cliyok049@gmail.com>
2022-06-07 10:14:49 +0300

    Merge remote-tracking branch 'origin/master'

--- Program Flow.py
+++ Program Flow.py
@@ -11,3 +11,19 @@ if age >= 18:
 else:
     print("You have {} years to join the brotherhood of the secret society".format(18-age))
     print("You are welcome to read about the brotherhood")
+#in Python true is 1 and false is zero and in conditions any non zero or non-empty values evaluate to true
+
+
+# for loops
+number_characters = input("Please enter the number/ numbers or character/ characters of your choice ")
+cleaned_number_characters = " "
+for typed in range(0, len(number_characters))
+    if number_characters[typed] in name:
+        cleaned_number_characters += number_characters[typed]
+new_number_characters = cleaned_number_characters# this prevents the loop from repeating , making it perfom once through the loop
+print(new_number_characters)
+
+for char in number_characters:
+    if char in name:
+        cleaned_number_characters+=char
+print(cleaned_number_characters)
